90c8ad686203bc9a4ed954fbb74a1a883b356430
Ayehilton <lvalquin7@gmail.com>
2026-06-15 18:35:46 +0200

    Update messageCommandRouter.ts

--- artifacts/api-server/src/bot/handlers/messageCommandRouter.ts
+++ artifacts/api-server/src/bot/handlers/messageCommandRouter.ts
@@ -19,6 +19,7 @@ import { annonceMessage } from "./messageCommands/annonce.js";
 import { sondageMessage } from "./messageCommands/sondage.js";
 import { inviteMessage } from "./messageCommands/invite.js";
 import { welcomeMessage } from "./messageCommands/welcome.js";
+import { pubMessage } from "./messageCommands/pub.js";
 import { logger } from "../../lib/logger.js";
 
 export const PREFIX = "!";
@@ -78,6 +79,9 @@ export async function handleMessageCommand(message: Message) {
       case "invitations": await inviteMessage(message, args); break;
       case "welcome":
       case "bienvenue": await welcomeMessage(message, args); break;
+      case "pub":
+      case "ad":
+      case "promo": await pubMessage(message, args); break;
       default: break;
     }
   } catch (err) {
